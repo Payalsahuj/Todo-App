e8147fa32988baf421135191da0f4e4797c0cff2
Payal sahu <115584065+Payalsahuj@users.noreply.github.com>
2025-03-26 23:29:22 +0530

    API to delete todo

--- app/api/todos/[id]/route.ts
+++ app/api/todos/[id]/route.ts
@@ -15,6 +15,8 @@ export async function POST(request: Request) {
   const body = await request.json();
   const { title, description, priority, tags, assignedUsers, notes } = body;
 
+  //   TOdo: Add validation to check if assignedUsers are valid users
+
   const todo = new Todo({
     title,
     description,
@@ -24,5 +26,16 @@ export async function POST(request: Request) {
     notes,
   });
   await todo.save();
+  return Response.json({ data: todo });
+}
+
+export async function DELETE(request: Request) {
+  const params = new URL(request.url).pathname.split("/");
+  const id = params[params.length - 1];
+
+  const todo = await Todo.findByIdAndDelete(id);
+  if (!todo) {
+    return Response.json({ error: "Todo not found" }, { status: 404 });
+  }
   return Response.json({ data: todo });
 }
