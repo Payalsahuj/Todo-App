391a4a40c2f8958f88cf324ccd83ff7d5ed5bf3a
Payal sahu <115584065+Payalsahuj@users.noreply.github.com>
2025-03-26 18:35:26 +0530

    Added dropdown

--- todo-app/app/Component/Header.tsx
+++ todo-app/app/Component/Header.tsx
@@ -1,16 +1,29 @@
+import { useContext } from "react";
 import { Dropdown } from "./Dropdown";
 import { ExportButton } from "./ExportButton";
+import { todoContext } from "../page";
+import Image from "next/image";
 
 export const Header = () => {
+  const { currentUser } = useContext(todoContext);
   return (
     <header className="app-header">
       <h1>Todo List</h1>
       <div className="user-controls">
         <ExportButton />
 
-        <Dropdown options={[]} />
+        <Dropdown />
 
-        <div className="user-profile"></div>
+        <div className="user-profile">
+          <span className="username">{currentUser?.username}</span>
+          <Image
+            src={""}
+            alt="User avatar"
+            width={36}
+            height={36}
+            className="avatar"
+          />
+        </div>
       </div>
     </header>
   );
--- todo-app/app/page.tsx
+++ todo-app/app/page.tsx
@@ -1,13 +1,48 @@
 "use client";
-import { useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { Header } from "./Component/Header";
 
+// imake interface
+
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+}
+
+export const todoContext = createContext({
+  userList: [] as User[],
+  setUserList: (userList: User[]) => {},
+  currentUser: null as User | null,
+  setCurrentUser: (user: User) => {},
+});
+
 export default function Home() {
-  const [userList, setUserList] = useState([]);
+  const [userList, setUserList] = useState([] as User[]);
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    setUserList([
+      {
+        id: 1,
+        username: "payal",
+        email: "sahupayal220@gmail.com",
+      },
+    ]);
+    setCurrentUser({
+      id: 1,
+      username: "payal",
+      email: "sahupayal220@gmail.com",
+    });
+  }, []);
   return (
-    <div className=" app-container ">
-      <Header />
-      <main className="app-main">hb</main>
-    </div>
+    <todoContext.Provider
+      value={{ userList, setUserList, currentUser, setCurrentUser }}
+    >
+      <div className=" app-container ">
+        <Header />
+        <main className="app-main">hb</main>
+      </div>
+    </todoContext.Provider>
   );
 }
